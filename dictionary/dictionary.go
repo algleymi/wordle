acbef8fc598c579184622b8eac60722d3c6861c1
Arno Schutijzer <arno.schutijzer@skryv.com>
2022-02-03 21:49:30 +0100

    feat: only 5 letter words in dictionary

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -11,17 +11,6 @@ func GetWordList() []string {
 	return readWordListFromFile()
 }
 
-func GetFiveLetterWordList() []string {
-	words := readWordListFromFile()
-	var filteredWords []string
-	for _, word := range words {
-		if len(word) == 5 {
-			filteredWords = append(filteredWords, word)
-		}
-	}
-	return filteredWords
-}
-
 func readWordListFromFile() []string {
 	data, _ := ioutil.ReadFile("./words_alpha.txt")
 	text := string(data)
